ae9da4d42a4a9e1ec068222adcfbdcc143f7f261
Akshat Naruka <79619620+AkshatNaruka@users.noreply.github.com>
2022-11-09 12:01:54 +0530

    day12

---
+++ hierarchy/rainbowTabs/rainbowTabs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rainbowTabs
+//
+//  Created by admin on 09/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem.badgeValue = nil
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
